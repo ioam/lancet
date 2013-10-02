3d7bab0d38d3d89076d85b70edf1c45c960a59a9
jlstevens <jlstevens[at]ed.ac.uk>
2013-10-02 17:39:21 +0100

    Docstring fixes in __init__.py

--- lancet/__init__.py
+++ lancet/__init__.py
@@ -1,53 +1,53 @@
 """
-Lancet consists of three fundamental class types: Argument Specifiers, Command
-Templates and Launchers. Launch Helpers are useful helper functions for using
-Lancet.
+Lancet consists of three fundamental class types: Argument Specifiers,
+Command Templates and Launchers. The review_and_launch decorator is a
+helper utility that helps coordinate the use of these objects.
 
 Argument Specifiers
 -------------------
 
-Argument specifiers are intended to be clear, succinct and composable way of
-specifying large parameter sets. High-dimensional parameter sets are typical of
-large-scale scientific models and can make managing such models and simulations
-difficult. Using argument specifiers, you can document how the parameters of
-your model vary across runs without long, flat lists of arguments or requiring
-deeply nested loops.
+Argument specifiers are intended to offer a succinct, declarative and
+composable way of specifying large parameter sets. High-dimensional
+parameter sets are typical of large-scale scientific models and can
+make specifying such models and simulations difficult. Using argument
+specifiers, you can document how the parameters of your model vary
+across runs without extended lists of arguments or requiring deeply
+nested loops.
 
-Argument specifiers can be freely intermixed with Python code, simplifying the
-use of scientific software with a Python interface. They also invoke commandline
-programs using command template objects to build commands and launchers to
-execute them. In this way they can help simplify the management of simulation,
-analysis and visualisation tools with Python.
-
-Typical usage includes specification of constant arguments, description of
-parameter ranges or even search procedures (such as hillclimbing, bisection
-optimisation etc). Argument specifiers can compose together using Cartesian
-Products or Concatenation to express huge numbers of argument sets concisely.
+Argument specifiers can be freely intermixed with Python code,
+simplifying the use of scientific software with a Python
+interface. They also invoke commandline programs using CommandTemplate
+objects to build commands and Launcher objects to execute
+them. Argument specifiers can compose together using Cartesian
+Products or Concatenation to express huge numbers of argument sets
+concisely. In this way they can help simplify the management of
+simulation, analysis and visualisation tools with Python.
 
 Command Templates
 ------------------
 
-When working with external tools, a command template is needed to turn an
-argument specifier into an executable command. These objects are designed to be
-customisable with sensible defaults to handle as much boilerplate as
-possible. They may require argument specifiers with a certain structure (certain
-argument names and value types) to operate correctly.
+When working with external tools, a command template is needed to turn
+an argument specifier into an executable command. These objects are
+designed to be customisable with sensible defaults to reduce
+boilerplate. They may require argument specifiers with a certain
+structure (certain argument names and value types) to operate
+correctly.
 
 Launchers
 ---------
 
-A launcher is designed to execute commands on a given computational platform,
-making use of as much concurrency where possible. The default Launcher class
-executes commands locally while the QLauncher class is designed to launch
-commands on Sun Grid Engine clusters.
+A Launcher is designed to execute commands on a given computational
+platform, making use of as much concurrency where possible. The
+default Launcher class executes commands locally while the QLauncher
+class is designed to launch commands on Sun Grid Engine clusters.
 
-Launch Helpers
---------------
+The review_and_launch decorator
+-------------------------------
 
-These helper functions help verify correct usage of lancet for specific
-use-cases. They check for consistent useage automatically and offer an in-depth
-review of all settings specified before launch. The goal is to help users
-identify mistakes early before consuming computational time and resources.
+This decorator helps codify a pattern of Lancet use that checks for
+consistency and offers an in-depth review of all settings before
+launch. The goal is to help users identify mistakes early before
+consuming computational time and resources.
 """
 
 from lancet.core import *
