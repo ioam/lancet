6869c75a08e4a07c22112ed1c450ec42a0fba131
jlstevens <jlstevens@ed.ac.uk>
2014-06-14 21:05:42 +0100

    Added exclude option to Args.show method to omit specific keys

--- lancet/core.py
+++ lancet/core.py
@@ -350,15 +350,19 @@ class Args(Arguments):
         return [seen.setdefault(idfun(e),e) for e in sequence
                 if idfun(e) not in seen]
 
-    def show(self):
+    def show(self, exclude=[]):
         """
         Convenience method to inspect the available argument values in
         human-readable format. The ordering of keys is determined by
         how quickly they vary.
+
+        The exclude list allows specific keys to be excluded for
+        readability (e.g. to hide long, absolute filenames).
         """
         ordering = self.constant_keys + self.varying_keys
         spec_lines = [', '.join(['%s=%s' % (k, s[k]) for k in ordering
-                                 if k in s]) for s in self.specs]
+                                 if (k in s) and (k not in exclude)])
+                      for s in self.specs]
         print('\n'.join(['%d: %s' % (i,l) for (i,l) in enumerate(spec_lines)]))
 
 
