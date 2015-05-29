276bff9bfac79f558eb91c8ac24e600371c627bd
jlstevens <jlstevens@ed.ac.uk>
2015-05-30 00:26:06 +0100

    Added the FileInfo.from_pattern convenience classmethod
    This classmethod makes it easier to chain the pattern matches of a
    FilePattern in order to create a new FileInfo instance.

--- lancet/core.py
+++ lancet/core.py
@@ -871,6 +871,25 @@ class FileInfo(Args):
         self.pprint_args(['source', 'key', 'filetype'], ['ignore'])
 
 
+    @classmethod
+    def from_pattern(cls, pattern, filetype=None, key='filename', root=None, ignore=[]):
+        """
+        Convenience method to directly chain a pattern processed by
+        FilePattern into a FileInfo instance.
+
+        Note that if a default filetype has been set on FileInfo, the
+        filetype argument may be omitted.
+        """
+        filepattern = FilePattern(key, pattern, root=root)
+        if FileInfo.filetype and filetype is None:
+            filetype = FileInfo.filetype
+        elif filetype is None:
+            raise Exception("The filetype argument must be supplied unless "
+                            "an appropriate default has been specified as "
+                            "FileInfo.filetype")
+        return FileInfo(filepattern, key, filetype, ignore=ignore)
+
+
     @property
     def table(self):
         """
