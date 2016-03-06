25b703968ba368a7cfc047503f1891f453cafc5e
Philipp Rudiger <P.Rudiger@ed.ac.uk>
2016-03-06 02:08:49 +0000

    Fixed to_table function to match new holoviews column signature

--- lancet/core.py
+++ lancet/core.py
@@ -36,9 +36,11 @@ def to_table(args, vdims=[]):
     "Helper function to convet an Args object to a HoloViews Table"
     if not Table:
         return "HoloViews Table not available"
-    kdims = args.constant_keys + args.varying_keys
-    items = ((tuple([spec[k] for k in kdims]), ()) for spec in args.specs)
-    return Table(items, kdims=kdims, vdims=[]).reindex(None, vdims)
+    kdims = [dim for dim in args.constant_keys + args.varying_keys
+             if dim not in vdims]
+    items = [tuple([spec[k] for k in kdims+vdims])
+             for spec in args.specs]
+    return Table(items, kdims=kdims, vdims=vdims)
 
 #=====================#
 # Argument Specifiers #
